e06498678cdf2fd123a010b310708df749525f54
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-05-02 12:04:42 +1000

    Separated menu with main scene. Placeholder until we get VO to time everything.

---
+++ BlindGameJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    // Runs during initialisation.
+    private void Start()
+    {
+        Invoke("ChangeScene", 25f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.anyKey)
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
